86da75791266a2910bf5beb87028772795a99726
Andreas von Studnitz <avs@integer-net.de>
2023-09-07 15:47:19 +0200

    Observe order data changes (creation and update)

--- Observer/SalesOrderSaveAfterObserver.php
+++ Observer/SalesOrderSaveAfterObserver.php
@@ -8,9 +8,10 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\MessageQueue\PublisherInterface;
 use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Sales\Model\Order;
 use MageOS\AsyncEvents\Helper\QueueMetadataInterface;
 
-class CustomerSaveAfterObserver implements ObserverInterface
+class SalesOrderSaveAfterObserver implements ObserverInterface
 {
     private Json $json;
     private PublisherInterface $publisher;
@@ -24,14 +25,19 @@ class CustomerSaveAfterObserver implements ObserverInterface
     }
 
     /**
-     * @see @event customer_save_after
+     * @see @event sales_order_save_after
      */
     public function execute(Observer $observer)
     {
-        /** @var Customer $customer */
-        $customer = $observer->getEvent()->getData('customer');
-        $arguments = ['customerId' => $customer->getId()];
-        $data = ['customer.updated', $this->json->serialize($arguments)];
+        /** @var Order $order */
+        $order = $observer->getEvent()->getData('order');
+        $arguments = ['id' => $order->getIncrementId()];
+
+        $eventIdentifier = 'sales.order.updated';
+        if (empty($order->getOrigData('entity_id'))) {
+            $eventIdentifier = 'sales.order.created';
+        }
+        $data = [$eventIdentifier, $this->json->serialize($arguments)];
 
         $this->publisher->publish(
             QueueMetadataInterface::EVENT_QUEUE,
